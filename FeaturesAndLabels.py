cfaaeb95a9f1be19f5c79c15a693dd67d9e27db1
ross39 <Rheaney26@gmail.com>
2018-02-13 21:10:36 +0000

    Add files via upload
    Tutorial 1 -4

--- FeaturesAndLabels.py
+++ FeaturesAndLabels.py
@@ -1,6 +1,11 @@
 import pandas as pd
-import Quandl
-df = Quandl.get('WIKI/GOOGL')
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+
+
+df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close']*100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']*100.0
@@ -10,7 +15,8 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col ='Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.01*len(df)))#increasing this yields a higher accuracy
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
@@ -20,10 +26,11 @@ y = np.array(df['label'])
 X = preprocessing.scale(X)
 y = np.array(df['label'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)#20%percent of data used for training and testing 
 
 clf= LinearRegression()
+#clf=svm.SVR(change kernel if you want...kernel = 'poly)This is a different algorithm but yields a much worse accuracy result in this case. SVR means support vector regression
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+print(accuracy)#accuracy will be the squared error
